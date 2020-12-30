01dd8a5e32c292ebd827a7787c868ff99750c75c
Leksiooon <sierocinskip.maciej@gmail.com>
2020-12-30 22:24:51 +0100

    Utworzenie Klasy Samochód

---
+++ Komis/Models/MockSamochodRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Komis.Models
+{
+    public class MockSamochodRepository : ISamochodRepository
+    {
+        private List<Samochod> samochody;
+
+        public MockSamochodRepository()
+        {
+            if (samochody == null)
+            {
+                ZaladujSamochody();
+            }
+        }
+
+        private void ZaladujSamochody()
+        {
+            samochody = new List<Samochod>
+            {
+                new Samochod { Id=1, Marka="Ford", Model="Mustang", RokProdukcji=2016, Przebieg = "34 000 km", Pojemnosc="4 900 cm3", RodzajPaliwa="benzyna", Moc="421 KM", Cena=2450000, Opis="Najlepszy samochód ever.", ZdjecieUrl="to/jest/to/miejsce.jpg", MiniaturkaUrl="to/jest/to/miejsce.jpg", JestSamochodemTygodnia= true },
+                new Samochod { Id=2, Marka="Opel", Model="Vectra", RokProdukcji=2016, Przebieg = "34 000 km", Pojemnosc="4 900 cm3", RodzajPaliwa="benzyna", Moc="421 KM", Cena=2450000, Opis="Najlepszy samochód ever.", ZdjecieUrl="to/jest/to/miejsce.jpg", MiniaturkaUrl="to/jest/to/miejsce.jpg", JestSamochodemTygodnia= true },
+                new Samochod { Id=3, Marka="Toyota", Model="Corolla", RokProdukcji=2016, Przebieg = "34 000 km", Pojemnosc="4 900 cm3", RodzajPaliwa="benzyna", Moc="421 KM", Cena=2450000, Opis="Najlepszy samochód ever.", ZdjecieUrl="to/jest/to/miejsce.jpg", MiniaturkaUrl="to/jest/to/miejsce.jpg", JestSamochodemTygodnia= true },
+                new Samochod { Id=4, Marka="Skoda", Model="Octavia", RokProdukcji=2016, Przebieg = "34 000 km", Pojemnosc="4 900 cm3", RodzajPaliwa="benzyna", Moc="421 KM", Cena=2450000, Opis="Najlepszy samochód ever.", ZdjecieUrl="to/jest/to/miejsce.jpg", MiniaturkaUrl="to/jest/to/miejsce.jpg", JestSamochodemTygodnia= true },
+                new Samochod { Id=5, Marka="Audi", Model="A6", RokProdukcji=2016, Przebieg = "34 000 km", Pojemnosc="4 900 cm3", RodzajPaliwa="benzyna", Moc="421 KM", Cena=2450000, Opis="Najlepszy samochód ever.", ZdjecieUrl="to/jest/to/miejsce.jpg", MiniaturkaUrl="to/jest/to/miejsce.jpg", JestSamochodemTygodnia= true },
+                new Samochod { Id=6, Marka="Mercedes", Model="Benz", RokProdukcji=2016, Przebieg = "34 000 km", Pojemnosc="4 900 cm3", RodzajPaliwa="benzyna", Moc="421 KM", Cena=2450000, Opis="Najlepszy samochód ever.", ZdjecieUrl="to/jest/to/miejsce.jpg", MiniaturkaUrl="to/jest/to/miejsce.jpg", JestSamochodemTygodnia= true }
+            };
+        
+        }
+
+        public Samochod PobierzSamochodOId(int samochodId)
+        {
+            return samochody.FirstOrDefault(s => s.Id == samochodId);
+        }
+
+        public IEnumerable<Samochod> PobierzWszystkieSamochody()
+        {
+            return samochody;
+        }
+    }
+}
--- Komis/Startup.cs
+++ Komis/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Komis.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,6 +16,7 @@ namespace Komis
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ISamochodRepository, MockSamochodRepository>();
             services.AddMvc();
         }
 
