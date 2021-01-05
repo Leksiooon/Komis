bc57556f307053aec91419d7a3ee8bbfffe7ded0
Leksiooon <sierocinskip.maciej@gmail.com>
2021-01-05 14:27:17 +0100

    Dodanie widoków

---
+++ Komis/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Komis.Models;
+using Komis.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Komis.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISamochodRepository _samochodRepository;
+
+        public HomeController(ISamochodRepository samochodRepository)
+        {
+            _samochodRepository = samochodRepository;
+        }
+        public IActionResult Index()
+        {
+            var samochody = _samochodRepository.PobierzWszystkieSamochody().OrderBy(s => s.Marka);
+
+            var homeVM = new HomeVM()
+            {
+                Title = "Przegląd samochodów",
+                Samochody = samochody.ToList()
+            };
+
+            return View(homeVM);
+        }
+    }
+}
--- Komis/Startup.cs
+++ Komis/Startup.cs
@@ -1,12 +1,7 @@
 ﻿using Komis.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Komis
 {
